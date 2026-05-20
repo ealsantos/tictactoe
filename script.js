03801a1777d470046a3a5897f2620d4c52f6c474
ealsantos <212905821+ealsantos@users.noreply.github.com>
2026-05-20 16:57:09 +0100

    add check winner and change player turn logic

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ function createPlayer (name, marker){
 
 function gameManagement(player1, player2){
     let isPlayerOneTurn
-
     const winningConditions = [
         [[0,0], [0,1], [0,2]],
         [[1,0], [1,1], [1,2]],
@@ -36,6 +35,16 @@ function gameManagement(player1, player2){
         [[0,0], [1,1], [2,2]],
         [[0,2], [1,1], [2,0]],
     ]
+
+    function checkWinner(marker){
+        let hasWinner = false
+        winningConditions.forEach((item) => {
+            if (gameSetup.board[item[0][0]][item[0][1]] === marker && gameSetup.board[item[1][0]][item[1][1]] === marker && gameSetup.board[item[2][0]][item[2][1]] === marker){
+                hasWinner = true
+            }
+        });
+        return hasWinner
+    }
 
     function setFirstTurn(){
         return Math.floor((Math.random() * 2)) === 0
@@ -49,10 +58,20 @@ function gameManagement(player1, player2){
         isPlayerOneTurn = !isPlayerOneTurn
     }
 
+    function afterMove(marker){
+        if (checkWinner(marker)){
+            console.log('reset game')
+        } else {
+            console.log('switching turn')
+            switchTurn()
+        }
+    }
+
     return {
         startGame: initGame,
         currentTurn: function(){ return isPlayerOneTurn },
-        changePlayerTurn: switchTurn
+        changePlayerTurn: switchTurn,
+        verifyWinner: afterMove
     }
 }
 
@@ -60,14 +79,13 @@ const player1 = createPlayer('Emanuel', 'X')
 const player2 = createPlayer('Test', 'O')
 
 const game = gameManagement(player1, player2)
+const board = boardManagement()
 game.startGame()
+// simulate player 1 winning top row
 console.log(game.currentTurn())
-game.changePlayerTurn()
+board.selectPosition('X', 0, 0)
+board.selectPosition('X', 0, 1)
+board.selectPosition('X', 0, 1)
+
+game.verifyWinner(player1.marker)
 console.log(game.currentTurn())
-
-
-const test = boardManagement();
-test.selectPosition('X', 2, 0)
-console.log(gameSetup.board)
-
-console.log(gameSetup.board[0][0])
