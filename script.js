33c356e19bb2afbdf85ba8f7196c0da8d6fae3ba
ealsantos <212905821+ealsantos@users.noreply.github.com>
2026-05-25 18:19:01 +0100

    add DOM manipulation rendering logic

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ function boardManagement(){
             placeMarker(player.marker, rowIndex, columnIndex)
             game.verifyWinner(player)
         } else {
-            console.log('already taken')
+            renderGame.showGameResult(`This slot is already taken!`)
         } 
-    }
+
+        renderGame.getTurnInfo()
+    }   
 
     function resetGame(){
         gameSetup.board = [[1,2,3],[4,5,6],[7,8,9]]
@@ -79,33 +81,103 @@ function gameManagement(player1, player2){
         isPlayerOneTurn = !isPlayerOneTurn
     }
 
-    function setLoserFirst(winner){ isPlayerOneTurn = winner === player1 ? false : true
-        console.log(winner)
-    }
+    function setLoserFirst(winner){ isPlayerOneTurn = winner === player1 ? false : true}
 
     function afterMove(player){
         if (checkWinner(player.marker)){
             player.playerScore++
+            renderGame.showGameResult(`${player.name} won!`)
             board.resetGame()
+            renderGame.playerScoresRendering(player1, player2)
             setLoserFirst(player)
+
         } else if (!checkDraw()){
+            renderGame.showGameResult(`It's a draw!`)
             board.resetGame()
             switchTurn()
-        } 
+
+        } else {
+            switchTurn()
+        }
+        renderGame.gameRendering(gameSetup.board)
     }
 
     return {
         startGame: initGame,
         currentTurn: function(){ return isPlayerOneTurn },
-        verifyWinner: afterMove
+        verifyWinner: afterMove,
+        getActivePlayer: function(){ return isPlayerOneTurn ? player1 : player2 }
     }
 }
 
+const gameContainer = document.querySelector('.gameBoard')
+
+function displayManagement(){
+
+    function gameRendering(currentBoard) {
+        gameContainer.innerHTML = ''
+        for (let i = 0; i < currentBoard.length; i++) {
+            for (let j = 0; j < currentBoard[i].length; j++) {
+                const gamePositions = document.createElement("div");
+                gamePositions.textContent = currentBoard[i][j]
+                gameContainer.appendChild(gamePositions)
+
+                const activePlayer = game.currentTurn() ? player1 : player2
+
+                gamePositions.addEventListener("click", function() {
+                    board.makeMove(activePlayer, i, j)
+                });
+            }
+        }
+    }
+
+    function playerScoresRendering(player1, player2){
+        const playerOneScore = document.getElementById("player1Score")
+        const playerTwoScore = document.getElementById("player2Score")
+
+        playerOneScore.textContent = `${player1.name}: ${player1.playerScore}`
+        playerTwoScore.textContent = `${player2.name}: ${player2.playerScore}`
+    }
+
+
+    const firstTurnText = document.getElementById("firstTurnText")
+
+    function getTurnInfo(){
+        let currentPlayer = game.getActivePlayer()
+        firstTurnText.textContent = `Currently Playing: ${currentPlayer.name}`
+    }
+
+    function showGameResult(message){
+        const dialog = document.getElementById("gameMessage");
+        const closeButton = document.getElementById("closeMessage")
+        const statement = document.getElementById("messageText");
+
+        statement.textContent = message
+
+        dialog.showModal()
+        closeButton.addEventListener("click", () => {
+        dialog.close();
+            });
+
+    }
+
+    return {
+        gameRendering,
+        playerScoresRendering,
+        getTurnInfo,
+        showGameResult
+    }
+}
+
+// game tests
 const player1 = createPlayer('Emanuel', 'X')
 const player2 = createPlayer('Test', 'O')
 
 const game = gameManagement(player1, player2)
 const board = boardManagement()
+const renderGame = displayManagement()
 
-// Setup
 game.startGame()
+renderGame.gameRendering(gameSetup.board)
+renderGame.playerScoresRendering(player1, player2)
+renderGame.getTurnInfo()
